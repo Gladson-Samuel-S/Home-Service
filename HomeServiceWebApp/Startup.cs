de9b0afcd9105165ae5bfc3447c04fa1505a41d9
Gladson-Samuel-S <samuelgladson4@gmail.com>
2022-05-06 23:51:36 +0530

    Initial Commit.

---
+++ HomeServiceWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeServiceWebApp.Startup))]
+namespace HomeServiceWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
